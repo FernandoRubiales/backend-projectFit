ed530a89ce399c2c5948d584b1dfea50638ebed7
FernandoRubiales <fernandorubiales7@gmail.com>
2026-03-24 23:02:12 -0300

    Monitoring of the entity "Reserva"

--- src/main/java/com/projectFit/fit_api/repository/ReservaRepository.java
+++ src/main/java/com/projectFit/fit_api/repository/ReservaRepository.java
@@ -2,10 +2,29 @@ package com.projectFit.fit_api.repository;
 
 import com.projectFit.fit_api.entity.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ReservaRepository extends JpaRepository <Reserva, Long> {
+
+    //Query para verificar si el socio ya reservo esa clase antes
+    Optional<Reserva> reservaPorSocioyClaseId(@Param("socioId") Long socioId,
+                                              @Param("claseId") Long claseId);
+
+    //Query para verificar si el socio tiene reservas del mismo tipo de actividad para el mismo dia
+    List<Reserva> reservasPorDiayTipoActividad(@Param("socioId") Long socioId,
+                                               @Param("diaSemana") String diaSemana,
+                                               @Param("tipoActividadId") Long tipoActividadId);
+
+    //Query para verificar si el socio tiene otra reserva en el mismo horario para el mismo dia
+    List<Reserva> reservasMismoHorario(@Param("socioId") Long socioId,
+                                       @Param("diaSemana") String diaSemana,
+                                       @Param("horaInicio") String horaInicio,
+                                       @Param("horaFin") String horaFin);
 
 
 }
--- src/main/java/com/projectFit/fit_api/services/ReservaService.java
+++ src/main/java/com/projectFit/fit_api/services/ReservaService.java
@@ -3,12 +3,19 @@ package com.projectFit.fit_api.services;
 import com.projectFit.fit_api.dto.ReservaRequestDTO;
 import com.projectFit.fit_api.dto.ReservaResponseDTO;
 import com.projectFit.fit_api.entity.Clase;
+import com.projectFit.fit_api.entity.Reserva;
 import com.projectFit.fit_api.entity.Socio;
+import com.projectFit.fit_api.entity.SocioPlan;
+import com.projectFit.fit_api.mappers.ReservaMapper;
 import com.projectFit.fit_api.repository.ClaseRepository;
 import com.projectFit.fit_api.repository.ReservaRepository;
+import com.projectFit.fit_api.repository.SocioPlanRepository;
 import com.projectFit.fit_api.repository.SocioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +23,8 @@ public class ReservaService {
     private final ReservaRepository reservaRepository;
     private final SocioRepository socioRepository;
     private final ClaseRepository claseRepository;
+    private final SocioPlanRepository socioPlanRepository;
+    private final ReservaMapper reservaMapper;
 
     //REALIZAR UNA RESERVA
     public ReservaResponseDTO realizarReserva(ReservaRequestDTO reservaRequestDTO, String auth0Id){
@@ -25,6 +34,43 @@ public class ReservaService {
         Clase clase = claseRepository.findById(reservaRequestDTO.getClaseId())
                 .orElseThrow(() -> new RuntimeException("Clase no encontrada"));
 
-        
+        SocioPlan socioPlan = socioPlanRepository.planActivoporSocioyActividadId(socio.getId(), clase.getTipoActividad())
+                .orElseThrow(() -> new RuntimeException("No tenés un plan activo para esta actividad"));
+
+        //Validar que le queden clases disponibles del plan para reservar
+        if(socioPlan.getClasesDisponibles() <= 0){
+            throw new RuntimeException("No tenés clases disponibles en este plan");
+        }
+        //Validar que no haya hecho una reserva en esa clase ya
+        reservaRepository.reservaPorSocioyClaseId(socio.getId(), clase.getId())
+                .ifPresent(r -> {
+            throw new RuntimeException("Ya tenés una reserva para esta clase");
+        });
+
+        //Validar que no tenga reserva del mismo tipo de actividad, para el mismo dia
+        List<Reserva> reservasMismaActividad = reservaRepository.reservasPorDiayTipoActividad(
+                socio.getId(), clase.getDiaSemana(), clase.getTipoActividad().getId());
+
+        if(!reservasMismaActividad.isEmpty()){
+            throw new RuntimeException("Ya tenés una reserva de " + clase.getTipoActividad().getNombreTipoActividad() + " ese día");
+        }
+        //Validar que en el mismo horario no tenga otra reserva ese dia
+        List<Reserva> reservasMismoHorario = reservaRepository.reservasMismoHorario(
+                socio.getId(), clase.getDiaSemana(), clase.getHoraFin().toString(), clase.getHoraInicio().toString());
+
+        if (!reservasMismoHorario.isEmpty()) {
+            throw new RuntimeException("Ya tenés una reserva en ese horario");
+        }
+        //Validar que hayan cupos disponibles para la clase que quiere reservar
+        int cuposDisponibles = claseRepository.cuposDisponibles(clase.getId());
+        if(cuposDisponibles <= 0){
+            throw new RuntimeException("No hay mas cupos disponibles para esta clase");
+        }
+        Reserva reserva = new Reserva();
+        reserva.setClase(clase);
+        reserva.setSocioPlan(socioPlan);
+        reserva.setFechaHoraReserva(LocalDateTime.now());
+
+        return reservaMapper.toResponse(reservaRepository.save(reserva));
     }
 }
